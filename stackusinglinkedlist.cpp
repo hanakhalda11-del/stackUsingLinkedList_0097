32713700cfb264a84a2fa1c60c8b7d18f022df7c
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-14 18:54:47 +0700

    Refactor stack implementation using linked list

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -95,4 +95,9 @@ int main()
             cin >> value;
             stack.push(value);
             break;
-        
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            

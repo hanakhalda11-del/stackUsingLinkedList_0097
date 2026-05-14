93bb20616ecfce6164af54c79a9aeaf0f1872906
hanakhalda11-del <hanakhalda11@gmail.com>
2026-05-14 18:55:34 +0700

    Refactor stack implementation using linked list

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -105,4 +105,24 @@ int main()
                 cout << "Stack is empty. Cannot pop." << endl;
             }
             break;
-        
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                cout << "Stack is empty.No top value." << endl;
+            }
+            break;
+
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
+        }
+        cout << endl;
+    }
+    
